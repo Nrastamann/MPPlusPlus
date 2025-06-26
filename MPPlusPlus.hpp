ad6452a8f0fd9019743c67b1dba62c88f61d5dfc
Nrastamann <nicita351@Yandex.ru>
2025-06-27 00:39:06 +0300

    Feature(basic features) Added some basic functions, added type-erasure for functions map

--- MPPlusPlus.hpp
+++ MPPlusPlus.hpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <cstdlib>
 #include <unordered_map>
+#include <memory>
+#include <functions.hpp>
 
 enum TokenType
 {
@@ -19,9 +21,52 @@ enum TokenType
     Error,
 };
 
-static std::unordered_map<std::string, std::variant<double, int>> functions;
+class FunctionPointer
+{
+public:
+    template <typename T>
+    FunctionPointer(const T &t) : held_(std::make_unique<T>(t)) {}
+
+    template <typename U>
+    U cast() const
+    {
+        if (typeid(U) != held_->type_info())
+            throw std::runtime_error("Bad any cast");
+        return static_cast<holder<U> *>(held_)->t_;
+    }
+
+private:
+    struct base_holder
+    {
+        virtual ~base_holder() {}
+        virtual const std::type_info &type_info() const = 0;
+    };
+    template <typename T>
+    struct holder : base_holder
+    {
+        holder(const T &t) : t_(t) {}
+        const std::type_info &type_info() const
+        {
+            return typeid(t_);
+        }
+        T t_;
+    };
+
+private:
+    std::unique_ptr<base_holder> held_;
+};
+
+static const std::unordered_map<std::string, FunctionPointer> functions = {
+    {"+", FunctionPointer(&sum)},
+    {"-", FunctionPointer(&sub)},
+    {"*", FunctionPointer(&mult)},
+    {"/", FunctionPointer(&divd)},
+};
+
 // https://stackoverflow.com/questions/45715219/store-functions-with-different-signatures-in-a-map
 static std::string TokenTypeToString(TokenType token) throw();
+
+double strtod();
 
 struct Token
 {
@@ -36,6 +81,7 @@ class MathParser
     Token current_t = Token();
     std::string_view working_str = "";
     std::string_view::const_iterator iter = working_str.cbegin();
+
 public:
     MathParser(const std::string &working_str) : current_t(Token()), working_str(working_str), iter(this->working_str.begin()) {}
 
---
+++ functions.hpp
@@ -0,0 +1,10 @@
+double sum(double a, double b) { return a + b; }
+double divd(double a, double b) { return a / b; }
+double mult(double a, double b) { return a * b; }
+double sub(double a, double b) { return a - b; }
+double neg(double a) { return -a; }
+// double (double a, double b) { return a / b; }
+// double (double a, double b) { return a + b; }
+// double (double a, double b) { return a / b; }
+// double (double a, double b) { return a + b; }
+// double (double a, double b) { return a / b; }
